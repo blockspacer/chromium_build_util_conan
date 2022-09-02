1489b62efd1d588394ad9468954b10197fddfe06
denis <blockspacer@ya.ru>
2022-09-02 13:16:19 +0300

    UPD: conanfile

--- conanfile.py
+++ conanfile.py
@@ -88,24 +88,26 @@ class chromium_build_util_conan_project(ConanFile):
         return cmake
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
+        with tools.vcvars(self.settings, only_diff=False): # https://github.com/conan-io/conan/issues/6577
+            self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+            cmake = self._configure_cmake()
+            cmake.install()
 
     def build(self):
-        cmake = self._configure_cmake()
-        if self.settings.compiler == 'gcc':
-            cmake.definitions["CMAKE_C_COMPILER"] = "gcc-{}".format(
-                self.settings.compiler.version)
-            cmake.definitions["CMAKE_CXX_COMPILER"] = "g++-{}".format(
-                self.settings.compiler.version)
+        with tools.vcvars(self.settings, only_diff=False): # https://github.com/conan-io/conan/issues/6577
+            cmake = self._configure_cmake()
+            if self.settings.compiler == 'gcc':
+                cmake.definitions["CMAKE_C_COMPILER"] = "gcc-{}".format(
+                    self.settings.compiler.version)
+                cmake.definitions["CMAKE_CXX_COMPILER"] = "g++-{}".format(
+                    self.settings.compiler.version)
 
-        #cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = 'conan_paths.cmake'
+            #cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = 'conan_paths.cmake'
 
-        # The CMakeLists.txt file must be in `source_folder`
-        cmake.configure(source_folder=".")
-        cmake.build()
-        #cmake.test()
+            # The CMakeLists.txt file must be in `source_folder`
+            cmake.configure(source_folder=".")
+            cmake.build()
+            #cmake.test()
 
     # Importing files copies files from the local store to your project.
     def imports(self):
